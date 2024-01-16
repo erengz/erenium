f3daf3e87d6053eb60f131213213a39bfb93138f
senpailegal <senpailegal@protonmail.com>
2024-01-16 09:18:46 +0300

    modified:   package-lock.json 	modified:   package.json 	modified:   pages/_app.tsx 	private/

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,3 +1,4 @@
+import { Analytics } from '@vercel/analytics/react';
 import type { AppProps } from "next/app";
 import {
   ThirdwebProvider,
@@ -13,16 +14,21 @@ import Header from "../components/header";
 // You can also import additional chains from `@thirdweb-dev/chains` and pass them directly.
 const activeChain = "mumbai";
 
+
+
+
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <ThirdwebProvider
       clientId={process.env.NEXT_PUBLIC_TEMPLATE_CLIENT_ID}
       activeChain={activeChain}
     >
+      <Analytics />
       <Header />
       <Component {...pageProps} />
       <Navbar />
     </ThirdwebProvider>
+    
   );
 }
 
